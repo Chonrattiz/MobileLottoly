48308bbba054970f900fe1239a7d24db1d514bbf
poder <66011212129@gmail.com>
2025-09-21 15:17:27 +0700

    แก้

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   // เก็บ URL หลักไว้ที่นี่
-  static const String baseUrl = 'http://192.168.6.1:8080';
+  static const String baseUrl = 'https://api-oracel999.onrender.com';
 
   // ในอนาคตอาจมีค่าอื่นๆ เช่น
   // static const String apiKey = 'your_super_secret_api_key';
